8a3c1cc2235aa32c026e85cd98006d7741f94dd0
jaybill <2399599130@qq.com>
2017-01-19 22:56:31 +0800

    2017/1/19 23.00

--- src/main/java/com/jaybill/billblog/email/EmailUtil.java
+++ src/main/java/com/jaybill/billblog/email/EmailUtil.java
@@ -10,6 +10,9 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 邮件工具类
  * @author jaybill
@@ -17,29 +20,37 @@ import javax.mail.internet.MimeMessage;
  */
 public class EmailUtil {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmailUtil.class);
+	
 	public static String submitEmail(String receiveMailAccount) throws Exception{
+		LOGGER.debug("读取我的邮箱的配置文件");
 		//读取配置文件，获得我的邮箱的账号密码
 		Properties myEmailPro = new Properties();
 		myEmailPro.load(new FileInputStream("E:/eclipse_workplace/billblog-common/src/main/java/com/jaybill/billblog/email/my-email-info.properties"));
 		String myEmailAccount = myEmailPro.getProperty("myEmailAccount");
 		String myEmailPassword = myEmailPro.getProperty("myEmailPassword");
 		String myEmailSMTPHost = myEmailPro.getProperty("myEmailSMTPHost");
+		LOGGER.debug("创建参数配置, 用于连接邮件服务器的参数配置");
 		// 1. 创建参数配置, 用于连接邮件服务器的参数配置
         Properties props = new Properties();                    // 参数配置
         props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
         props.setProperty("mail.host", myEmailSMTPHost);        // 发件人的邮箱的 SMTP 服务器地址
         props.setProperty("mail.smtp.auth", "true");            // 请求认证，参数名称与具体实现有关
+        LOGGER.debug("根据配置创建会话对象, 用于和邮件服务器交互");
         // 2. 根据配置创建会话对象, 用于和邮件服务器交互
         Session session = Session.getDefaultInstance(props);
         session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
         // 3. 创建一封邮件
         //生成6位验证码
+        LOGGER.debug("生成6位验证码");
         String arr = "2345678abcdefhjkmnpqrstuvwxyz"; 
         StringBuilder code = new StringBuilder();
         for(int i=0;i<6;i++){
         	code.append(arr.charAt(new Random().nextInt(arr.length()-1)));
         }
+        LOGGER.debug("将6位验证码作为内容，创建一封邮箱");
         MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount,code.toString());
+        LOGGER.debug("根据 Session 获取邮件传输对象Transport，发送邮件");
         // 4. 根据 Session 获取邮件传输对象
         Transport transport = session.getTransport();
         // 5. 使用 邮箱账号 和 密码 连接邮件服务器
@@ -49,6 +60,7 @@ public class EmailUtil {
         transport.sendMessage(message, message.getAllRecipients());
         // 7. 关闭连接
         transport.close();
+        LOGGER.error("关闭Transport失败");
         //返回验证码
         return code.toString();
     }
