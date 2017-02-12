0dbf0b5046a3c2f012a99670681f2dde188635f1
jaybill <2399599130@qq.com>
2017-02-12 22:25:23 +0800

    增加一个类，专门存放静态字段，各种网址名称

---
+++ src/main/java/com/jaybill/billblog/website/WebsiteNames.java
@@ -0,0 +1,14 @@
+package com.jaybill.billblog.website;
+/**
+ * 网址名称
+ * @author jaybill
+ *
+ */
+public class WebsiteNames {
+
+	//域名
+	public static final String webSite = "http://jaybill.tunnel.qydev.com/";
+	//项目名
+	public static final String project = "billblog-manager-controller/";
+	public static final String userInfoPage = webSite+project+"userinfocontroller/touserinfo.do?userId=";
+}
